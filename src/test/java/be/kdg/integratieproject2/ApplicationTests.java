1443a978d65b104f6f7d3bc996d1d1151b74e26f
Remz <remz.grges@student.kdg.be>
2018-02-07 11:51:24 +0100

    Test

--- src/test/java/be/kdg/integratieproject2/ApplicationTests.java
+++ src/test/java/be/kdg/integratieproject2/ApplicationTests.java
@@ -1,22 +1,28 @@
 package be.kdg.integratieproject2;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
+
+/*
 @RunWith(SpringJUnit4ClassRunner.class)
 
-@ContextConfiguration(locations = {"../../../../resources/testcontest.xml"})
+@ContextConfiguration(locations = {"testcontext.xml"})
+*/
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(locations = {"classpath*:testcontext.xml"})
 public class ApplicationTests {
 
 	@Test
 	public void simpleTest() {
-		Assert.assertEquals(10,10);
+		assertThat("lol" , is("lol"));
 	}
 
 }
