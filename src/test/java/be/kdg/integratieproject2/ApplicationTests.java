2cd95c791f3a1d6fa32937b02c45bc3e8b8fd75c
Remz <remz.grges@student.kdg.be>
2018-02-07 11:21:21 +0100

    Test

--- src/test/java/be/kdg/integratieproject2/ApplicationTests.java
+++ src/test/java/be/kdg/integratieproject2/ApplicationTests.java
@@ -1,16 +1,22 @@
 package be.kdg.integratieproject2;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+
+@ContextConfiguration(locations = {"../../../../resources/testcontest.xml"})
+
 public class ApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void simpleTest() {
+		Assert.assertEquals(10,10);
 	}
 
 }
