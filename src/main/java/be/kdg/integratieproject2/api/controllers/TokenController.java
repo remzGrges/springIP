39f48ef310ca6d41af92571ba50f07b6fe1b14c0
Remz <remz.grges@student.kdg.be>
2018-03-12 19:14:17 +0100

    tokenRepo

--- src/main/java/be/kdg/integratieproject2/api/controllers/TokenController.java
+++ src/main/java/be/kdg/integratieproject2/api/controllers/TokenController.java
@@ -2,6 +2,7 @@ package be.kdg.integratieproject2.api.controllers;
 
 import be.kdg.integratieproject2.api.BadRequestException;
 import be.kdg.integratieproject2.bussiness.Interfaces.TokenService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,15 @@ public class TokenController {
 
     public TokenController(TokenService service) {
         this.service = service;
+    }
+
+    @RequestMapping(value = "/create/{token}", method = RequestMethod.GET)
+    public ResponseEntity<String> getToken( Authentication authentication, @PathVariable("token") String token ) throws BadRequestException
+    {
+
+        String soort = service.getTokenSort(token);
+        return new ResponseEntity<String>( soort, HttpStatus.OK);
+
     }
 }
 
