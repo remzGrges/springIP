f9c88de85d9e7aef0f2d1f653baa3cd7c5fa9a5a
Indy De Wacker <indy.dewacker@gmail.com>
2018-03-21 13:34:42 +0100

    commit

--- src/main/java/be/kdg/integratieproject2/api/controllers/WebSocketController.java
+++ src/main/java/be/kdg/integratieproject2/api/controllers/WebSocketController.java
@@ -25,4 +25,6 @@ public class WebSocketController {
     public OutputMessage send(@DestinationVariable String sessionId, InputMessage msg) {
         return sessionService.addMessageToSession(sessionId, msg);
     }
+
+
 }
